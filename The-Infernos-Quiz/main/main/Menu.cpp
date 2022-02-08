51f194962f02fb753916dfbf1d9d29d9d328c6d4
Stanimir Dimitrov <SMDimitrov19@codingburgas.bg>
2022-02-08 21:27:15 +0200

    Adding an option tab and adding an actual option to change the color, and creating a fresh menu for them both.

--- The-Infernos-Quiz/main/main/Menu.cpp
+++ The-Infernos-Quiz/main/main/Menu.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 
-//Used to replace 'system("cls")'
+//Used to replace 'system("cls")' in order to reduce flickering
 void clearScreen()
 {
 	COORD cursorPosition;
@@ -20,6 +20,7 @@ void clearScreen()
 	cursorPosition.Y = 0;
 
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+
 }
 
 //Prints Main menu
@@ -51,10 +52,161 @@ void menu(string arrow, int arrowPos) {
 	else
 	cout << ":                 Exit                     :" << endl;
 	cout << ":                                          :" << endl;
-	cout << ":     Use Space or Enter to choose         :" << endl;
+	cout << ":        / Use Enter to choose /           :" << endl;
 	cout << ":                                          :" << endl;
 	cout << "*------------------------------------------*" << endl;
 }
+
+void options(string arrow, int arrowPos) {
+	cout << "*--------------------------------------------------------------*" << endl;
+	cout << ":                                                              :" << endl;
+	cout << ":  _______  _______  _______  ___   _______  __    _  _______  :" << endl;
+	cout << ": |       ||       ||       ||   | |       ||  |  | ||       | :" << endl;
+	cout << ": |   _   ||    _  ||_     _||   | |   _   ||   |_| ||  _____| :" << endl;
+	cout << ": |  | |  ||   |_| |  |   |  |   | |  | |  ||       || |_____  :" << endl;
+	cout << ": |  |_|  ||    ___|  |   |  |   | |  |_|  ||  _    ||_____  | :" << endl;
+	cout << ": |       ||   |      |   |  |   | |       || | |   | _____| | :" << endl;
+	cout << ": |_______||___|      |___|  |___| |_______||_|  |__||_______| :" << endl;
+	cout << ":                                                              :" << endl;
+	cout << ":                                                              :" << endl;
+	if (arrowPos == 0)
+		cout << ":                       " << arrow << "  Color                             :" << endl;
+	else
+		cout << ":                            Color                             :" << endl;
+	cout << ":                                                              :" << endl;
+	if (arrowPos == 1)
+		cout << ":                       " << arrow << "  Back                              :" << endl;
+	else
+		cout << ":                            Back                              :" << endl;
+	cout << ":                                                              :" << endl;
+	cout << ":                   / Use Enter to choose /                    :" << endl;
+	cout << ":                                                              :" << endl;
+	cout << "*--------------------------------------------------------------*" << endl;
+}
+void colorMenu(string arrow, int arrowPos) {
+	cout << "*---------------------------------------------------------*" << endl;
+	cout << ":                                                         :" << endl;
+	cout << ":  _______  _______  ___      _______  ______    _______  :" << endl;
+	cout << ": |       ||       ||   |    |       ||    _ |  |       | :" << endl;
+	cout << ": |       ||   _   ||   |    |   _   ||   | ||  |  _____| :" << endl;
+	cout << ": |       ||  | |  ||   |    |  | |  ||   |_||_ | |_____  :" << endl;
+	cout << ": |      _||  |_|  ||   |___ |  |_|  ||    __  ||_____  | :" << endl;
+	cout << ": |     |_ |       ||       ||       ||   |  | | _____| | :" << endl;
+	cout << ": |_______||_______||_______||_______||___|  |_||_______| :" << endl;
+	cout << ":                                                         :" << endl;
+	cout << ":                                                         :" << endl;
+	if (arrowPos == 0)
+		cout << ":                     " << arrow << "  Aqua                           :" << endl;
+	else
+		cout << ":                          Aqua                           :" << endl;
+	cout << ":                                                         :" << endl;
+	if (arrowPos == 1)
+		cout << ":                     " << arrow << "  Green                          :" << endl;
+	else
+		cout << ":                          Green                          :" << endl;
+	cout << ":                                                         :" << endl;
+	if (arrowPos == 2)
+		cout << ":                     " << arrow << "  Red                            :" << endl;
+	else
+		cout << ":                          Red                            :" << endl;
+	cout << ":                                                         :" << endl;
+	if (arrowPos == 3)
+		cout << ":                     " << arrow << "  Purple                         :" << endl;
+	else
+		cout << ":                          Purple                         :" << endl;
+	cout << ":                                                         :" << endl;
+	if (arrowPos == 4)
+		cout << ":                     " << arrow << "  White                          :" << endl;
+	else
+		cout << ":                          White                          :" << endl;
+	cout << ":                                                         :" << endl;
+	if (arrowPos == 5)
+		cout << ":                     " << arrow << "  Back                           :" << endl;
+	else
+		cout << ":                          Back                           :" << endl;
+	cout << ":                                                         :" << endl;
+	cout << ":                 / Use Enter to choose /                 :" << endl;
+	cout << ":                                                         :" << endl;
+	cout << "*---------------------------------------------------------*" << endl;
+}
+int chooseColor(string arrow, int arrowPos) {
+	arrowPos = 0;
+	while (1) {
+		colorMenu(arrow, arrowPos);
+		switch (_getch()) {
+		case 32:
+		case '\r':
+			if (arrowPos == 0) {
+				system("color 3");
+			}
+			else if (arrowPos == 1) {
+				system("color 2");
+			}
+			else if (arrowPos == 2) {
+				system("color 4");
+			}
+			else if (arrowPos == 3) {
+				system("color 5");
+			}
+			else if (arrowPos == 4) {
+				system("color 7");
+			}
+			else if (arrowPos == 5) {
+				system("cls");
+				return 0;
+			}
+			break;
+		case KEY_UP:
+		case 'W':
+		case 'w':
+			if (arrowPos != 0)
+				arrowPos--;
+			break;
+		case KEY_DOWN:
+		case 'S':
+		case 's':
+			if (arrowPos != 5)
+				arrowPos++;
+			break;
+		}
+		clearScreen();
+	}
+}
+
+int chooseOptions(string arrow, int arrowPos) {
+	arrowPos = 0;
+	while (1) {
+		options(arrow, arrowPos);
+		switch (_getch()) {
+		case 32:
+		case '\r':
+			if (arrowPos == 0) {
+				system("cls");
+				chooseColor(arrow, arrowPos);
+				arrowPos = 0;
+			}
+			else if (arrowPos == 1) {
+				system("cls");
+				return 0;
+			}
+			break;
+		case KEY_UP:
+		case 'W':
+		case 'w':
+			if (arrowPos != 0)
+				arrowPos--;
+			break;
+		case KEY_DOWN:
+		case 'S':
+		case 's':
+			if (arrowPos != 2)
+				arrowPos++;
+			break;
+		}
+		clearScreen();
+	}
+}
+
 
 //Choose whether to Play, change Options or Exit
 int chooseMenu(string arrow, int arrowPos) {
@@ -63,7 +215,21 @@ int chooseMenu(string arrow, int arrowPos) {
 		switch (_getch()) {
 		case 32:
 		case '\r':
-			if (arrowPos == 2) {
+			if (arrowPos == 0) {
+				system("cls");
+				//chooseDifficulty(arrow, arrowPos, size);
+				switch (arrowPos) {
+				case 3: arrowPos = 0; break;
+				default: return 0; break;
+				}
+			}
+			else if (arrowPos == 1) {
+				system("cls");
+				chooseOptions(arrow, arrowPos);
+				arrowPos = 0;
+			}
+			else if (arrowPos == 2) {
+				system("cls");
 				exit(0);
 			}
 			break;
